0d6219008ca9b7b5e17df8d93b8b6c2fffb4ba23
Itumeleng <thiotsoela@gmail.com>
2020-10-15 00:57:08 +0200

    Getting a single account

--- Acounts.js
+++ Acounts.js
@@ -1,15 +1,18 @@
 const accounts = [
     {
+        "id": 1,
         "name": "John Doe",
         "email" : "john@gmail.com",
         "username": "johntheman"
     },
     {
+        "id": 2,
         "name": "Jane Doe",
         "email" : "Jane@gmail.com",
         "username": "jannie"
     },
     {
+        "id": 3,
         "name": "Sam Dean",
         "email" : "winch@gmail.com",
         "username": "win45"
--- app.js
+++ app.js
@@ -9,11 +9,34 @@ const app = express();
 // Init middleware
 app.use(logger)
 
+// Getting all accounts 
 app.get('/api/accounts', (req, res) => {
     // if we want to return JSON file
     res.send(accounts)
 })
 
+
+
+// getting asingle account based on the ID
+app.get('/api/accounts/:id', (req, res) => {
+    const found = accounts.some(function(person){
+        return person.id === parseInt(req.params.id)
+    })
+
+    // Take if the requested Id exist or not and return a status 400 if t does not exist
+    if (found){
+        //res.send(req.params.id)
+        res.json(accounts.filter(function(person){
+            // We have to parse it becasue it is tring
+            return person.id === parseInt(req.params.id);
+        }))
+    }
+    else{
+        res.status(400).json({message: `There is no account with an id of ${req.params.id}`})
+    }
+
+    
+})
 
 // The static website
 app.use(express.static(path.join(__dirname, "public")))
